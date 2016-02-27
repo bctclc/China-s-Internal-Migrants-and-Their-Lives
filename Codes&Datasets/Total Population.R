9457c419505ad5480e786520476423f4766d22ed
bctclc <cl1007@georgetown.edu>
2016-02-27 11:19:40 -0500

    TOTAL POPULATION

--- Codes&Datasets/Total Population.R
+++ Codes&Datasets/Total Population.R
@@ -17,7 +17,17 @@ positions <- c("Guangdong","Zhejiang","Jiangsu","Shanghai","Beijing","Fujian","S
                "Jilin","Gansu","Hainan","Ningxia","Qinghai","Tibet")
 ggplot(melt_barchart) +aes(x=X.1, y=value, fill=variable) +
   geom_bar(stat="identity") +
-  scale_fill_manual(values=c("#05968f", "#07e2d8")) +
+  scale_fill_manual(values=c("#05968f", "#4fc2bd")) +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border= element_blank())+
+  scale_x_discrete(limits = positions)
+
+### subset for lollipop chart
+loll <- subset(file[-1,], select = c(X.1, percentage.change))
+
+### plot lollipop chart
+ggplot(loll, aes(y=loll$percentage.change, x=loll$X.1)) + 
+  geom_point(stat="identity", shape=24, size=3, color="#005955", fill="#005955")+
   theme(panel.background = element_rect(fill = "white"),
         panel.border= element_blank())+
   scale_x_discrete(limits = positions)
