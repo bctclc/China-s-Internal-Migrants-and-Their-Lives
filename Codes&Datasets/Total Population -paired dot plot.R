c9a50b24157faabfb3aea02fdf9d3e4b55fe935c
bctclc <cl1007@georgetown.edu>
2016-03-10 20:22:58 -0500

    alternative for total population

---
+++ Codes&Datasets/Total Population -paired dot plot.R
@@ -0,0 +1,30 @@
+setwd("E:\\—ßœ∞œ‡πÿ\\R\\China-s-Internal-Migrants-and-Their-Lives\\Codes&Datasets")
+library(ggplot2)
+library(reshape2)
+
+#data cleanup
+mydata <- read.csv("total population.csv")
+mydata <- subset(mydata, mydata$X.1!="National Average")
+mydata$difference <- NULL
+mydata$percentage.change <- NULL
+
+melt_data <- melt(mydata)
+mydata <- read.csv("total population.csv")
+mydata$X2000 <- NULL
+mydata$X2010  <- NULL
+mydata$Region <- NULL
+mydata$X.1 <- NULL
+melt_data <- merge(melt_data, mydata, by = "X")
+
+#paired dot plot
+positions <- c("Tibet","Qinghai","Ningxia","Gansu","Jilin",
+               "Heilongjiang","Hainan","Henan","Jiangxi","Anhui",
+               "Hunan","Guizhou","Guangxi",
+               "Shanxi","Chongqing","Shaanxi","Hubei","Sichuan","Yunan",
+               "Hebei","Inner Mongolia","Liaoning","Xinjiang","Shandong",
+               "Tianjin","Fujian","Beijing","Jiangsu","Shanghai","Zhejiang","Guangdong")
+ggplot(melt_data, aes(x = value, y = X.1))+
+  geom_path(group = melt_data$X.1, size=2,aes(color=percentage.change) )+
+  geom_point(size = 3, shape=".") +
+  scale_y_discrete(limits = positions)
+  
